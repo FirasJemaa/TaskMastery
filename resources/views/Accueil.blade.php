f3023f5a0754deaa2bc3dbb7ea5d1ff1cb8070c5
Firas Jemaa <104427054+FirasJemaa@users.noreply.github.com>
2023-10-02 09:20:58 +0200

    utilisation de js et css dans ressources

--- resources/views/Accueil.blade.php
+++ resources/views/Accueil.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/accueil.css') }}" rel="stylesheet">
+    <!--<link href="{{ asset('css/accueil.css') }}" rel="stylesheet">-->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Task Mastery</title>
 </head>
 <body>
