f662eb60ff242d0ad2d7d0c76d786db3e458bf6f
Philippe Kurz <philippe.kurz@gmail.com>
2023-11-14 11:52:48 +0100

    modif migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('pseudo');
-            $table->string('email')->unique();
+            $table->string('name')->nullable(false);
+            $table->string('surname')->nullable(true);
+            $table->string('pseudo')->nullable(true);
+            $table->string('email')->nullable(false)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
