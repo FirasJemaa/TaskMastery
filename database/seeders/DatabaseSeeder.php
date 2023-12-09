8fc223e953de9261124aff9434fd084dce5516b7
Firas Jemaa <firas.jemaa@yahoo.fr>
2023-12-09 08:25:13 +0100

    seeder statuts fait

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(CouleursSeeder::class);
+        $this->call(StatutSeeder::class);
 
 
 
--- database/seeders/StatutSeeder.php
+++ database/seeders/StatutSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StatutSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class StatutSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('statuts')->insert([
+            'designation'  => ucfirst('En attente')
+        ]);
+        DB::table('statuts')->insert([
+            'designation'  => ucfirst('En cours')
+        ]);
+        DB::table('statuts')->insert([
+            'designation'  => ucfirst('Terminé')
+        ]);
     }
 }
