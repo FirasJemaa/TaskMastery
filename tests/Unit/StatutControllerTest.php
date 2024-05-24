1238be3274a643b986cdbe17863e43a35ce77dd5
Firas Jemaa <firas.jemaa@yahoo.fr>
2024-05-24 17:02:26 +0200

    dernier push

--- tests/Unit/StatutControllerTest.php
+++ tests/Unit/StatutControllerTest.php
@@ -4,18 +4,25 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use App\Http\Controllers\StatutController;
+use App\Models\Statut;
 
 class StatutControllerTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
     public function test_index(): void
-    {
-        $statut = new StatutController();
-        $this->assertEquals(
-            view("projets.indexTache", compact("statuts")),
-            $statut->index()
-        );
-    }
+{
+    $statutController = new StatutController();
+
+    // Obtenez les statuts depuis la base de données avec le même ordre que dans le contrôleur
+    $statuts = Statut::all()->sortBy("id");
+
+    // Appel de la méthode index du contrôleur StatutController
+    $response = $statutController->index();
+
+    // Vérifiez que la vue retournée est celle attendue
+    $this->assertEquals(
+        view("projets.indexTache", compact("statuts")),
+        $response
+    );
+}
+
 }
